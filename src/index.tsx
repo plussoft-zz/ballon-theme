bf6219308a0b9acfd371691614fba70ddbb3f85a
Cleber Gomes <fclebinho@gmail.com>
2020-02-02 10:04:57 -0300

    Ajustes

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
 export { default as Button } from "./button-component";
 export { default as Input } from "./input-component";
-export { default } from "./theme-component";
+export { default as theme } from "./theme-component";
