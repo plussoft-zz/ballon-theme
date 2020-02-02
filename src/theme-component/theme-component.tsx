6262ea56c257fb7bce47ec2248aabae57231c06a
Cleber Gomes <fclebinho@gmail.com>
2020-02-02 12:54:59 -0300

    Ajustes

--- src/theme-component/theme-component.tsx
+++ src/theme-component/theme-component.tsx
@@ -3,13 +3,9 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export default createMuiTheme({
   props: {
     // Name of the component ⚛️
-    MuiTab: {
+    MuiButtonBase: {
       // The default props to change
       disableRipple: true
-    },
-    MuiButtonBase: {
-      // The properties to apply
-      disableRipple: true // No more ripple, on the whole application!
     }
   }
 });
